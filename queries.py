3d7e33cc6f0e96559e93d3dc92d21e0bc0ed12b4
Aston Haycock <astonhaycocks@gmail.com>
2025-02-21 18:03:24 -0700

    cli

--- queries.py
+++ queries.py
@@ -1,25 +1,31 @@
 def add_account(db, username, email):
     db.execute("INSERT INTO accounts (username, email) VALUES (?,?)", (username, email))
+    db.commit()
 
 def create_user(db, email, first_name, last_name):
     db.execute("INSERT INTO users (email, first_name, last_name) VALUES (?,?,?)", 
               (email, first_name, last_name))
+    db.commit()
 
 def create_post(db, username, message, posted_at):
     db.execute("INSERT INTO posts (username, message, posted_at) VALUES (?,?,?)",
               (username, message, posted_at))
+    db.commit()
 
 def create_comment(db, post_id, username, message, posted_at):
     db.execute("INSERT INTO comments (post_id, username, message, posted_at) VALUES (?,?,?,?)",
               (post_id, username, message, posted_at))
+    db.commit()
 
 def create_like(db, username, post_id):
     db.execute("INSERT INTO likes (username, post_id) VALUES (?,?)",
               (username, post_id))
+    db.commit()
 
 def create_follow(db, follower, followee):
     db.execute("INSERT INTO follows (follower, followee) VALUES (?,?)",
               (follower, followee))
+    db.commit()
     
 
 def get_all(db, tables):
@@ -29,30 +35,36 @@ def get_all(db, tables):
             column_names = [description[0] for description in res.description]
             print(tabulate(data, headers=column_names, tablefmt="fancy_grid"))
 
+
 def add_account(db, username, email):
     db.execute("INSERT INTO accounts (username, email) VALUES (?,?)", (username, email))
+    db.commit()
 
 def create_user(db, email, first_name, last_name):
     db.execute("INSERT INTO users (email, first_name, last_name) VALUES (?,?,?)", 
                 (email, first_name, last_name))
+    db.commit()
     
 def create_post(db, username, message, posted_at):
     db.execute("INSERT INTO posts (username, message, posted_at) VALUES (?,?,?)",
                 (username, message, posted_at))
+    db.commit()
 def create_comment(db, post_id, username, message, posted_at):
     db.execute("INSERT INTO comments (post_id, username, message, posted_at) VALUES (?,?,?,?)",
                 (post_id, username, message, posted_at))
+    db.commit()
 
 def create_like(db, username, post_id):
     db.execute("INSERT INTO likes (username, post_id) VALUES (?,?)",
                 (username, post_id))
+    db.commit()
     
 
     
 def create_follow(db, follower, followee):
     db.execute("INSERT INTO follows (follower, followee) VALUES (?,?)",
                 (follower, followee))
-    
+    db.commit()
 
 def recommended_follow(db, username):
     query = """
--- testing.py
+++ testing.py
@@ -14,12 +14,13 @@ def Options():
   print('3. Post')
   print('4. Delete Post')
   print('5. comment')
-  print('6. delete comment')
+  print('6. update comment')
   print('7. Like')
   print('8. dislike')
   print('9. Follow')
   print('10. Unfollow')
-  print('11. Quit')
+  print('11. update post')
+  print('12. Quit')
   return input('Enter your choice: ')
 
 
@@ -33,11 +34,61 @@ while True:
     first_name = input('Enter first name: ')
     last_name = input('Enter last name: ')
     create_user(db, email, first_name, last_name)
+  
+  if(choice == 2):
+    username = input('Enter username: ')
+    email = input('Enter email: ')
+    add_account(db, username, email)
+  
+  if(choice == 3):
+    username = input('Enter username: ')
+    message = input('Enter message: ')
+    posted_at = datetime.now()
+    create_post(db, username, message, posted_at)
 
+  if(choice == 4):
+    pass
+    post_id = input('Enter post id: ')
+    # delete_post(db, post_id)  
 
+  if(choice == 5):
+    post_id = input('Enter post id: ')
+    username = input('Enter username: ')
+    message = input('Enter message: ')
+    posted_at = datetime.now()
+    create_comment(db, post_id, username, message, posted_at)
 
+  if(choice == 6):
+    pass
+    # comment_id = input('Enter comment id: ')
+    # delete_comment(db, comment_id)
 
+  if(choice == 7):
+    username = input('Enter username: ')
+    post_id = input('Enter post id: ')
+    create_like(db, username, post_id)
 
+  if(choice == 8):
+    pass
+    # username = input('Enter username: ')
+    # post_id = input('Enter post id: ')
+    # delete_like(db, username, post_id)
+
+  if(choice == 9):
+    follower = input('Enter follower: ')
+    followee = input('Enter followee: ')
+    create_follow(db, follower, followee)
+
+  if(choice == 10):
+    pass
+    # follower = input('Enter follower: ')
+    # followee = input('Enter followee: ')
+    # delete_follow(db, follower, followee)
 
   if choice == 11:
+    post_id = input('Enter post id: ')
+    message = input('Enter message: ')
+    update_post(db, post_id, message)
+
+  if choice == 12:
     break
